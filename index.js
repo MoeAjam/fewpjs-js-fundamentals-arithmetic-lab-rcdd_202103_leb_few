06e8989248ecf65db7675af0e24065f579b5360c
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-06 12:23:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ let num2 = 2;
 let num3 = 13;
 let num4 = 3;
 var multiply = num1 * num2;
-var random = Math.random();
+var random = Math.random() + 1;
 var mod = num3 % num4;
 var max = Math.max(20, 2, 3, 4, 6);
