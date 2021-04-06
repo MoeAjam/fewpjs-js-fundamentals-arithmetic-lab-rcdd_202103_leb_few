4cd8745058e6106edcb2c2dd3d28901294f58364
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-06 12:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // Write your code here
-let multiply = 31 * 2;
-let random = Math.random()+1;
-let mod = 13 % 3;
-let max = Math.max(20, 2, 3, 4, 6);
+var multiply = 31 * 2;
+var random = Math.random()+1;
+var mod = 13 % 3;
+var max = Math.max(20, 2, 3, 4, 6);
